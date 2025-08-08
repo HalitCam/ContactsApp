01fd22eee60e10848e1f7580f9ddbe0727174eab
Halit <xltcamm@gmailcom>
2025-08-08 21:44:22 +0200

    Form Constructer is built

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Form = () => {
+    const [form, setForm] = useState({ fullName: '', phoneNumber: '' })
+    const onChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+function onSubmit(){
+    console.log("Onclick edildi")
+}
     return (
         <div>
-            Form
+            <div>
+                <input name='fullName' placeholder='Full Name' onChange={onChangeInput} />
+            </div>
+            <div>
+                <input name='phoneNumber' placeholder='Phone Number' onChange={onChangeInput} />
+
+            </div>
+            <div>
+                <button onClick={onSubmit} >Add</button>
+            </div>
+
+            <h1>Isim : {form.fullName} <br /> soyisim: {form.phoneNumber}</h1>
         </div>
     );
 }
