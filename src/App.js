c28113d1f791509668c5a87eb7a8fcc2223af645
Halit <xltcamm@gmailcom>
2025-08-08 17:19:56 +0200

    first temple constructed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Contacts from "./components/Contacts"
 
 function App() {
   return (
     <div className="App">
-      
+      <Contacts/>
     </div>
   );
 }
